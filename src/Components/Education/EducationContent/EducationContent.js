270dfba0d6bdd4de81c0c1fb9ab8d1edbb6e014f
kathirvelusmart <kathirvelusmart@gmail.com>
2022-09-27 10:37:39 +0530

    Certifications Added

--- src/Components/Education/EducationContent/EducationContent.js
+++ src/Components/Education/EducationContent/EducationContent.js
@@ -1,15 +1,35 @@
 import React from 'react';
 import './EducationContent.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAward } from '@fortawesome/free-solid-svg-icons';
+import { certificatesArray } from '../../../constants/constant';
+
 
 function EducationContent() {
+    const certificates = certificatesArray.map((certificate) => {
+        return (
+            <div>
+                <FontAwesomeIcon icon={faAward} />
+                <span style={{ marginLeft: 10 }}>
+                    {certificate}
+                </span>
+            </div>
+        )
+    }
+    )
     return (
         <div className="educationContent">
             <h1>Education</h1>
             <h2>
-                B.E [CSE]
+                B.E [Computer Science Engineering...]
             </h2>
-            <span>Bachelor of Engineering</span>
-            <h3>M.A.M College of Engineering and Technology.</h3>
+            <h4>Graduated at M.A.M College of Engineering and Technology.</h4>
+            <h3>
+                Certifications
+            </h3>
+            <div className='certificate'>
+                {certificates}
+            </div>
         </div >
     )
 }
---
+++ src/constants/constant.js
@@ -0,0 +1,8 @@
+export const certificatesArray = [
+    "Web applications for Everybody, University of Michigan, Coursera.",
+    'Web Design for Everybody: Basics of Web Development & Coding , University of Michigan, Coursera.',
+    'Introduction to HTML5, University of Michigan, Coursera.',
+    'Introduction to CSS3,University of Michigan, Coursera .',
+    'Java Course With Practical Examples for Absolute Beginners- Udemy.',
+    'Object-Oriented Programming - Basics to Advance (Java OOPS)-Udemy.'
+]
